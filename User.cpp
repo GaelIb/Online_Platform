c5e04e983018324d171e5894b252a5055738a7ed
Perea094 <181926373+Perea094@users.noreply.github.com>
2025-05-22 18:12:42 -0600

    Create User.cpp

---
+++ User.cpp
@@ -0,0 +1,19 @@
+#include "User.h"
+
+#include <iostream>
+
+Profile::Profile() : username("None"), email("None") {}
+
+Profile::Profile(const std::string& _username, const std::string& _email)
+    : username(_username), email(_email) {}
+
+const std::string& Profile::getUsername() const {return username;}
+const std::string& Profile::getEmail() const {return email;}
+
+void Profile::setUsername(const std::string& _username) {username = _username;}
+void Profile::setEmail(const std::string& _email) {email = _email;}
+
+std::string Profile::displayProfile() const
+{
+    return "Username: " + username + "\nEmail: " + email;
+}
