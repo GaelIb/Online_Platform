ce374ccdea08a40d8c34f62d98e09b7a284c364f
Perea094 <181926373+Perea094@users.noreply.github.com>
2025-05-22 18:27:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "Instructor.h"
-// #include "Student.h"
+#include "Student.h"
 
 int main()
 {
     // Create an Student object
+    Student student("john_doe", "johndoe@example.com", "Math, Science");
 
+    // Create an Instructor object
+    Instructor instructor("drsmith", "drsmith@example.com", "Physics");
 
     return 0;
 }
