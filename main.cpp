f0d5d650330540651ca04a9d545077f16e1ac6b4
Perea094 <181926373+Perea094@users.noreply.github.com>
2025-05-22 18:41:54 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Instructor.h"
 #include "Student.h"
 
+#include <iostream>
+
 int main()
 {
     // Create an Student object
@@ -9,6 +11,11 @@ int main()
     // Create an Instructor object
     Instructor instructor("drsmith", "drsmith@example.com", "Physics", 10);
     Instructor instructor2("drdiaz", "drdiaz@example.com", "Spanish", 15);
+
+    // Display the profiles
+    std::cout << student.displayProfile() << std::endl;
+    std::cout << instructor.displayProfile() << std::endl;
+    std::cout << instructor2.displayProfile(true) << std::endl;
     return 0;
 }
 
